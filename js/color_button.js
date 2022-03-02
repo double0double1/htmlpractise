618ade4b060678b0b4bf8f36b694edb823214b11
double0double1 <connect.ismail@protonmail.com>
2022-03-02 17:31:40 +0530

    Initial comm ism

---
+++ js/color_button.js
@@ -0,0 +1,21 @@
+function show(){
+			x = document.getElementById("list")
+			y = document.getElementById("hi")
+			z = document.getElementById("sh")
+			
+			x.style.display = "block"
+			y.style.display = "inline"
+			z.style.display = "none"
+
+
+		}
+
+function hide(){
+			x = document.getElementById("list")
+			y = document.getElementById("hi")
+			z = document.getElementById("sh")
+
+			x.style.display = "none"
+			y.style.display = "none"
+			z.style.display = "inline"
+		}
